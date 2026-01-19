2a997e1ffbbe346dade3503b4eb89cd47d0e8dbc
cableyesto <dialkins@tutamail.com>
2026-01-19 15:36:55 +0100

    Done task 2. Adding CSS & Images.

---
+++ Webpack/task_2/webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './js/dashboard_main.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              disable: true, // webpack@2.x and newer
+              mozjpeg: {
+                progressive: true,
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
