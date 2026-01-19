ff632df4d9cb7c6638295a19b2c72d94b4c29c75
cableyesto <dialkins@tutamail.com>
2026-01-19 10:46:40 +0100

    Add the ready function to add text to the DOM.

--- Webpack/task_0/src/index.js
+++ Webpack/task_0/src/index.js
@@ -1,12 +1,7 @@
 import $ from 'jquery';
 
-$('body').append($('<p>').text('Holberton Dashboard'));
-
-$('body').append(
-  $('<p>')
-    .append('Dashboard ')
-    .append($('<b>').text('data for'))
-    .append(' the students')
-);
-
-$('body').append($('<p>').text('Copyright - Holberton School'));
+$(document).ready(function () {
+  $('body').append('<p>Holberton Dashboard</p>');
+  $('body').append('<p>Dashboard <b>data for</b> the students</p>');
+  $('body').append('<p>Copyright - Holberton School</p>');
+});
