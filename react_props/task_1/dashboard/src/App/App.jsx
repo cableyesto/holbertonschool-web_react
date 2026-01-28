93bdaf90cb63f1d3412a337ff7c99dcff4a0d945
cableyesto <dialkins@tutamail.com>
2026-01-28 16:24:25 +0100

    Done 1. Write the tests for each component.

---
+++ react_props/task_1/dashboard/src/App/App.jsx
@@ -0,0 +1,27 @@
+import './App.css'
+import { Fragment } from 'react'
+import Header from '../Header/Header.jsx'
+import Notifications from '../Notifications/Notifications.jsx'
+import Footer from '../Footer/Footer.jsx'
+import Login from '../Login/Login.jsx'
+
+function App() {
+  return (
+    <>
+      <div className='root-notifications'>
+        <Notifications />
+      </div>
+      <Fragment>
+        <Header />
+      </Fragment>
+      <Fragment>
+        <Login />
+      </Fragment>
+      <Fragment>
+        <Footer isIndex={true} />
+      </Fragment>
+    </>
+  )
+};
+
+export default App
