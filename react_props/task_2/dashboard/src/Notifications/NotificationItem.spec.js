c4e6fdc46d2fc44cb105fc08e8220d8a64945a8d
cableyesto <dialkins@tutamail.com>
2026-02-02 13:55:12 +0100

    Update the test file for NotificationItem.

--- react_props/task_2/dashboard/src/Notifications/NotificationItem.spec.js
+++ react_props/task_2/dashboard/src/Notifications/NotificationItem.spec.js
@@ -1,3 +1,4 @@
+/*
 import {render, screen} from '@testing-library/react'
 import NotificationItem from './NotificationItem.jsx'
 
@@ -30,3 +31,23 @@ test('should render urgent NotificationItem in red', () => {
   expect(liElement.dataset.notificationType).toBe('urgent')
   expect(liElement.style.getPropertyValue('color')).toBe('red')
 })
+*/
+
+import NotificationItem from './NotificationItem';
+import { render, screen } from '@testing-library/react';
+
+describe('NotificationItem', () => {
+  test('li has blue color and data-notification-type is default when type is default', () => {
+    render(<NotificationItem type="default" value="New course available" />);
+    const item = screen.getByRole('listitem');
+    expect(item).toHaveStyle({ color: 'blue' });
+    expect(item).toHaveAttribute('data-notification-type', 'default');
+  });
+
+  test('li has red color and data-notification-type is urgent when type is urgent', () => {
+    render(<NotificationItem type="urgent" value="New resume available" />);
+    const item = screen.getByRole('listitem');
+    expect(item).toHaveStyle({ color: 'red' });
+    expect(item).toHaveAttribute('data-notification-type', 'urgent');
+  });
+});
