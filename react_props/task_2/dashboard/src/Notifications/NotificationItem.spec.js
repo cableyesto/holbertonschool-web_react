197bf3bfd608d012bb863cbb550c872ca425a635
cableyesto <dialkins@tutamail.com>
2026-02-02 13:57:33 +0100

    test: clean up the testing file.

--- react_props/task_2/dashboard/src/Notifications/NotificationItem.spec.js
+++ react_props/task_2/dashboard/src/Notifications/NotificationItem.spec.js
@@ -1,40 +1,5 @@
-/*
-import {render, screen} from '@testing-library/react'
-import NotificationItem from './NotificationItem.jsx'
-
-test('should render default NotificationItem in blue', () => {
-  render(
-    <NotificationItem
-      type='default'
-      value='Bob'
-      html=''
-    />
-  )
-
-  const liElement = screen.getByText(/Bob/i)
-
-  expect(liElement.dataset.notificationType).toBe('default')
-  expect(liElement.style.getPropertyValue('color')).toBe('blue')
-})
-
-test('should render urgent NotificationItem in red', () => {
-  render(
-    <NotificationItem
-      type='urgent'
-      value='Bob'
-      html=''
-    />
-  )
-
-  const liElement = screen.getByText(/Bob/i)
-
-  expect(liElement.dataset.notificationType).toBe('urgent')
-  expect(liElement.style.getPropertyValue('color')).toBe('red')
-})
-*/
-
-import NotificationItem from './NotificationItem';
 import { render, screen } from '@testing-library/react';
+import NotificationItem from './NotificationItem.jsx';
 
 describe('NotificationItem', () => {
   test('li has blue color and data-notification-type is default when type is default', () => {
