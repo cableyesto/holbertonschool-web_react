0091fad5d1e3653d824ffdc27ac7c2f7acae634b
cableyesto <dialkins@tutamail.com>
2026-02-09 16:39:49 +0100

    fix: change the BodySectionWithMarginBottom component to accept the title as property and children passed to BodySection.

--- react_component/task_3/dashboard/src/BodySection/BodySectionWithMarginBottom.jsx
+++ react_component/task_3/dashboard/src/BodySection/BodySectionWithMarginBottom.jsx
@@ -1,11 +1,13 @@
 import BodySection from './BodySection.jsx'
 import './BodySectionWithMarginBottom.css'
 
-function BodySectionWithMarginBottom() {
+function BodySectionWithMarginBottom({title, children}) {
   return (
     <>
       <div className='bodySectionWithMargin'>
-        <BodySection title='Title' />
+        <BodySection title={title}>
+          {children}
+        </BodySection>
       </div>
     </>
   )
