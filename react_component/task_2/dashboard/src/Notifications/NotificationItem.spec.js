96221c2fae0c00315466afc6258d4681760373ed
cableyesto <dialkins@tutamail.com>
2026-02-09 15:50:18 +0100

    Done task 2. Handling Events.

---
+++ react_component/task_2/dashboard/src/Notifications/NotificationItem.spec.js
@@ -0,0 +1,37 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import NotificationItem from './NotificationItem.jsx';
+
+describe('NotificationItem', () => {
+  test('li has blue color and data-notification-type is default when type is default', () => {
+    render(<NotificationItem type="default" value="New course available" />);
+    const item = screen.getByRole('listitem');
+    expect(item).toHaveStyle({ color: 'blue' });
+    expect(item).toHaveAttribute('data-notification-type', 'default');
+  });
+
+  test('li has red color and data-notification-type is urgent when type is urgent', () => {
+    render(<NotificationItem type="urgent" value="New resume available" />);
+    const item = screen.getByRole('listitem');
+    expect(item).toHaveStyle({ color: 'red' });
+    expect(item).toHaveAttribute('data-notification-type', 'urgent');
+  });
+});
+
+// Clean up
+afterEach(() => {
+  jest.restoreAllMocks()
+})
+
+test('should called the prop function on click event', async () => {
+  const mockProp = jest.fn()
+
+  // render(<NotificationItem type="urgent" value="New resume available" markAdRead={() => {mockProp}} />);
+  render(<NotificationItem type="urgent" value="New resume available" markAsRead={mockProp} />);
+
+  const item = screen.getByRole('listitem');
+  const user = userEvent.setup()
+  await user.click(item)
+
+  expect(mockProp).toHaveBeenCalledTimes(1)
+})
---
+++ react_component/task_2/dashboard/src/main.jsx
@@ -0,0 +1,10 @@
+// import 'react-devtools'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App/App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App isLoggedIn={false} />
+  </StrictMode>,
+)
