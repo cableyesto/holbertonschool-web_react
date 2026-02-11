1f15fc468815b1f1d8cb33b25285261ec6f94052
cableyesto <dialkins@tutamail.com>
2026-02-11 10:02:17 +0100

    Revert last modifications.

--- react_styling/task_1/dashboard/src/CourseList/CourseListRow.jsx
+++ react_styling/task_1/dashboard/src/CourseList/CourseListRow.jsx
@@ -1,38 +1,31 @@
 function CourseListRow({ isHeader = false, textFirstCell = '', textSecondCell = null }) {
   if (isHeader) {
     return (
-      <tr>
-        {textSecondCell === null ? (
-          <th
-            colSpan={2}
-            className="bg-table-header border border-gray-400 opacity-66 px-2 py-1"
-          >
-            {textFirstCell}
-          </th>
-        ) : (
-          <>
-            <th className="bg-table-header border border-gray-400 opacity-66 px-2 py-1">
-              {textFirstCell}
-            </th>
-            <th className="bg-table-header border border-gray-400 opacity-66 px-2 py-1">
-              {textSecondCell}
-            </th>
-          </>
-        )}
-      </tr>
-    );
+      <>
+        <tr className='bg-table-header opacity-[66%]'>
+          {
+            textSecondCell === null ? (
+              <th className='border border-gray-400' colSpan={2}>{textFirstCell}</th>
+            ) : (
+              <>
+                <th className='border border-gray-400'>{textFirstCell}</th>
+                <th className='border border-gray-400'>{textSecondCell}</th>
+              </>
+            )
+          }
+        </tr>
+      </>
+    )
   } else {
     return (
-      <tr>
-        <td className="bg-table-rows border border-gray-400 pl-2 py-1 opacity-45">
-          {textFirstCell}
-        </td>
-        <td className="bg-table-rows border border-gray-400 pl-2 py-1 opacity-45">
-          {textSecondCell}
-        </td>
-      </tr>
-    );
+      <>
+        <tr className='bg-table-rows opacity-[45%]'>
+          <td className='border border-gray-400 pl-[8px]'>{textFirstCell}</td>
+          <td className='border border-gray-400 pl-[8px]'>{textSecondCell}</td>
+        </tr>
+      </>
+    )
   }
 }
 
-export default CourseListRow;
+export default CourseListRow
