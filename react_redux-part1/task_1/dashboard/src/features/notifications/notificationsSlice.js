469978b192ccefe991305803721f836e3f34789d
cableyesto <dialkins@tutamail.com>
2026-03-19 11:03:18 +0100

    Copy file to task_1.

---
+++ react_redux-part1/task_1/dashboard/src/features/notifications/notificationsSlice.js
@@ -0,0 +1,65 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { getLatestNotification } from '../../utils/utils';
+import axios from 'axios';
+
+const initialState = {
+  notifications: [],
+  displayDrawer: true
+}
+
+const API_BASE_URL = "http://localhost:5173";
+
+const ENDPOINTS = { notifications: `${API_BASE_URL}/notifications.json` };
+
+const fetchNotifications = createAsyncThunk(
+  'notifications/fetchNotifications',
+  async () => {
+    const response = await axios.get(ENDPOINTS.notifications);
+    const latestNotif = {
+      id: 3,
+      type: "urgent",
+      html: { __html: getLatestNotification() }
+    };
+
+    const currentNotifications = response.data.notifications;
+    const indexToReplace = currentNotifications.findIndex(
+      notification => notification.id === 3
+    );
+
+    const updatedNotifications = [...currentNotifications];
+    if (indexToReplace !== -1) {
+      updatedNotifications[indexToReplace] = latestNotif;
+    } else {
+      updatedNotifications.push(latestNotif);
+    }
+    return updatedNotifications;
+  }
+)
+
+const notificationsSlice = createSlice({
+  name: 'notifications',
+  initialState,
+  reducers: {
+    markNotificationAsRead: (state, action) => {
+      state.notifications = state.notifications.filter(
+        notification => notification.id !== action.payload
+      );
+      console.log(`Notification ${action.payload} has been marked as read`);
+    },
+    showDrawer: (state) => {
+      state.displayDrawer = true;
+    },
+    hideDrawer: (state) => {
+      state.displayDrawer = false;
+    }
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchNotifications.fulfilled, (state, action) => {
+      state.notifications = action.payload;
+    });
+  }
+});
+
+export const { markNotificationAsRead, showDrawer, hideDrawer } = notificationsSlice.actions;
+export { fetchNotifications };
+export default notificationsSlice.reducer;
